c5836822f26612fe54d7458a822d8d4c32d5f558
Inksie <mansell.steph@gmail.com>
2021-11-02 18:12:25 +0000

    Connected to Unsplash API to randomise bg image

--- src/App.js
+++ src/App.js
@@ -2,16 +2,51 @@ import "./App.css";
 import logo from "./assets/logo.svg";
 import logotext from "./assets/logo-text.svg";
 import ChatBox from "./components/ChatBox";
+import Logo from "./components/Logo";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [backgroundUrl, setBackgroundUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsplashKey = require("./unsplashKey.json");
+    let endpoint = `https://api.unsplash.com/photos/random?collections=OI9sDnoqGc8&fit&client_id=${unsplashKey}`;
+
+    fetch(endpoint)
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+        setBackgroundUrl(response.urls.full);
+        setLoading(false);
+      })
+      .catch(() => {
+        setBackgroundUrl(
+          "https://source.unsplash.com/1920x1080/?beach+city+nightlife"
+        );
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={logotext} className="logo-text" alt="logo" />
-        <ChatBox />
-      </header>
-    </div>
+    <>
+      {loading ? (
+        <h1>dd</h1>
+      ) : (
+        <div
+          className="App"
+          style={{ backgroundImage: `url(${backgroundUrl})` }}
+        >
+          <header className="App-header">
+            <Logo img={logo} className="App-logo" />
+            <Logo img={logotext} className="logo-text" />
+          </header>
+          <ChatBox />
+        </div>
+      )}
+    </>
   );
 }
 
