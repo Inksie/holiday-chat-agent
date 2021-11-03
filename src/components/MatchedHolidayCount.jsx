30ca25a1dddcd58bd782dbb55fb18b0d98e2dca6
Inksie <mansell.steph@gmail.com>
2021-11-03 11:50:59 +0000

    Added component to display holiday count in separate bubble

---
+++ src/components/MatchedHolidayCount.jsx
@@ -0,0 +1,19 @@
+function MatchedHolidayCount(props) {
+  if (props.length <= 0) {
+    return null;
+  } else {
+    const length = props.length;
+
+    const holidayLength =
+      length > 1 ? (
+        <div className="match-bubble">
+          I matched you with {length} holidays!
+        </div>
+      ) : (
+        <div className="match-bubble">I matched you with {length} holiday!</div>
+      );
+    return holidayLength;
+  }
+}
+
+export default MatchedHolidayCount;
