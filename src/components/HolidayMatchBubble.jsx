fbf0e8ba1d10d19ef1f97450c4771edb4001123b
Inksie <mansell.steph@gmail.com>
2021-11-03 11:50:13 +0000

    Fixed bug to dynamically render individual matched holiday

--- src/components/HolidayMatchBubble.jsx
+++ src/components/HolidayMatchBubble.jsx
@@ -1,30 +1,36 @@
+import { useState } from "react";
 import "./HolidayMatchBubble.css";
+import Modal from "./Modal";
 
 function HolidayMatchBubble(props) {
-  const length = props.recommendations.length;
+  const [modal, setModal] = useState(false);
+
+  function modalHandler() {
+    setModal(true);
+  }
+
+  function modalClose() {
+    setModal(false);
+  }
+
   return (
     <>
-      {length > 1 ? (
-        <div className="match-bubble">
-          You got matched with {length} holidays!
+      <div className="city-wrapper">
+        <div
+          onClick={modalHandler}
+          className="city-background"
+          style={{ backgroundImage: `url(${props.recommendation.img})` }}
+        ></div>
+        <div className="city-content">
+          <span className="city">
+            {props.recommendation.city}, {props.recommendation.country}!
+          </span>
         </div>
-      ) : (
-        <div>You got matched with {length} holiday!</div>
-      )}
+      </div>
 
-      {props.recommendations.map((holiday) => {
-        return (
-          <div className="city-wrapper">
-            <div
-              className="city-background"
-              style={{ backgroundImage: `url(${holiday.img})` }}
-            ></div>
-            <div className="city-content">
-              <span className="city">{holiday.city}!</span>
-            </div>
-          </div>
-        );
-      })}
+      {modal && (
+        <Modal close={modalClose} img={props.recommendation["random-image"]} />
+      )}
     </>
   );
 }
