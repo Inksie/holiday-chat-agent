1699ef48398c2c829005c31f733e184cf6e13276
Inksie <mansell.steph@gmail.com>
2021-11-04 09:54:41 +0000

    Removed loading state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [backgroundUrl, setBackgroundUrl] = useState("");
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsplashKey = require("./unsplashKey.json");
@@ -20,33 +19,22 @@ function App() {
       .then((response) => {
         console.log(response);
         setBackgroundUrl(response.urls.full);
-        setLoading(false);
       })
       .catch(() => {
         setBackgroundUrl(
           "https://source.unsplash.com/1920x1080/?beach+city+nightlife"
         );
-        setLoading(false);
       });
   }, []);
 
   return (
-    <>
-      {loading ? (
-        <h1>dd</h1>
-      ) : (
-        <div
-          className="App"
-          style={{ backgroundImage: `url(${backgroundUrl})` }}
-        >
-          <header className="App-header">
-            <Logo img={logo} className="App-logo" />
-            <Logo img={logotext} className="logo-text" />
-          </header>
-          <ChatBox />
-        </div>
-      )}
-    </>
+    <div className="App" style={{ backgroundImage: `url(${backgroundUrl})` }}>
+      <header className="App-header">
+        <Logo img={logo} className="App-logo" />
+        <Logo img={logotext} className="logo-text" />
+      </header>
+      <ChatBox />
+    </div>
   );
 }
 
